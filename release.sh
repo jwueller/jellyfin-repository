ff94779b4b0addb3d9fe1983d92f4709fa1d05c1
Johannes Wüller <johanneswueller@gmail.com>
2023-06-30 01:10:24 +0200

    Automatically install release script dependencies

--- release.sh
+++ release.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 <plugin-directory>"
-	exit 1
+    echo "Usage: $0 <plugin-directory>"
+    exit 1
+fi
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+source venv/bin/activate
+
+if ! command -v jprm &> /dev/null; then
+    pip install jprm
 fi
 
 artifact=$(jprm --verbosity=debug plugin build "$1") && {
-	jprm --verbosity=debug repo add \
-		--url="https://raw.githubusercontent.com/jwueller/jellyfin-repository/master" \
-		. \
-		"$artifact"
+    jprm --verbosity=debug repo add \
+        --url="https://raw.githubusercontent.com/jwueller/jellyfin-repository/master" \
+        . \
+        "$artifact"
 }
